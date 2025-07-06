1c71d6c2ac1c53666b019c8a5b4dc85fd1102a2e
Nahome Arere <nahom.arere@gmail.com>
2025-07-06 14:58:05 +0200

    normaler Mousecursor bei lightbox

--- js/cursor.js
+++ js/cursor.js
@@ -11,3 +11,17 @@ document.addEventListener("mousemove", (e) => {
     glow.style.left = `${x}px`;
     glow.style.top = `${y}px`;
 });
+
+// Lightbox2 Callback einrichten
+lightbox.option({
+    onOpen: function() {
+        cursor.style.display = 'none';
+        glow.style.display = 'none';
+        document.body.style.cursor = 'default';
+    },
+    onClose: function() {
+        cursor.style.display = 'block';
+        glow.style.display = 'block';
+        document.body.style.cursor = 'none';
+    }
+});
